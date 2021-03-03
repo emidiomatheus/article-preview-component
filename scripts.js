6c1c50fa3125f92e7f45a390477970ebb363279c
Matheus Emídio <emidiodeveloper@gmail.com>
2021-03-03 00:50:48 -0300

    finished layout

---
+++ scripts.js
@@ -0,0 +1,7 @@
+let popup = document.querySelector('.popup')
+let share = document.querySelector('.share')
+
+function myPopup() {
+    popup.classList.toggle("active")
+    share.classList.toggle("active")
+}
